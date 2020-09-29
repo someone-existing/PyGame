3c2992fa9c26abea9968bf8ac9b97b07b0419f28
someone_existing <66226290+someone-existing@users.noreply.github.com>
2020-09-29 14:44:30 +0200

    Added color to power up text
    made a new color variable supercolor, changing its value on picking up super power

--- game.py
+++ game.py
@@ -3,8 +3,10 @@ import random
 
 import threading
 import time
+
 # Import the pygame module
 import pygame, sys
+
 # Import pygame.locals for easier access to key coordinates
 # Updated to conform to flake8 and black standards
 from pygame.locals import *
@@ -12,12 +14,12 @@ from pygame.locals import *
 # Initialize pygame
 pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
-pygame.display.set_caption('Tinker Game')
+pygame.display.set_caption("Tinker Game")
 
 move_up_sound = pygame.mixer.Sound("Rising_putter.ogg")
 move_down_sound = pygame.mixer.Sound("Falling_putter.ogg")
 death_sound = pygame.mixer.Sound("Death.ogg")
-power_sound = pygame.mixer.Sound("Power.ogg")
+power_sound = pygame.mixer.Sound("power.ogg")
 
 move_up_sound.set_volume(0.5)
 move_down_sound.set_volume(0.5)
@@ -27,6 +29,7 @@ font = pygame.font.SysFont("Comic Sans", 50)
 gamefont = pygame.font.SysFont("Arial", 30)
 htpfont = pygame.font.SysFont("Comic Sans", 35)
 boja = (0, 0, 0)
+
 # Define a Player object by extending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player'
 # Define constants for the screen width and height
@@ -50,19 +53,20 @@ def draw_text(text, font, color, surface, x, y):
 
 
 pygame.mixer.music.load("main_menu.mp3")
-pygame.mixer.music.play(loops=-1) 
+pygame.mixer.music.play(loops=-1)
+
+
 def main_menu():
     click = False
     while True:
- 
+
         bg = pygame.image.load("bg.jpg")
         bg = pygame.transform.scale(bg, (800, 600))
-        screen.blit(bg, (0,0))
-        draw_text('Main Menu', font, (255, 255, 255), screen, 308, 20)
- 
+        screen.blit(bg, (0, 0))
+        draw_text("Main Menu", font, (255, 255, 255), screen, 308, 20)
+
         mx, my = pygame.mouse.get_pos()
 
- 
         button_1 = pygame.Rect(302, 102, 200, 50)
         button_2 = pygame.Rect(302, 202, 200, 50)
         button_3 = pygame.Rect(302, 302, 200, 50)
@@ -85,11 +89,10 @@ def main_menu():
         pygame.draw.rect(screen, (255, 0, 0), button_1)
         pygame.draw.rect(screen, (255, 0, 0), button_2)
         pygame.draw.rect(screen, (255, 0, 0), button_3)
-        draw_text('Play', font, (255, 255, 255), screen, 365, 110)
-        draw_text('Quit', font, (255, 255, 255), screen, 365, 210)
-        draw_text('How to play', font, (255, 255, 255), screen, 305, 310)
- 
- 
+        draw_text("Play", font, (255, 255, 255), screen, 365, 110)
+        draw_text("Quit", font, (255, 255, 255), screen, 365, 210)
+        draw_text("How to play", font, (255, 255, 255), screen, 305, 310)
+
         click = False
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -102,14 +105,16 @@ def main_menu():
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
- 
+
         pygame.display.update()
         clock.tick(60)
+
+
 def how_to_play():
     click = False
     while True:
-        white = (255,255,255)
-        black = (0,0,0)
+        white = (255, 255, 255)
+        black = (0, 0, 0)
         bg = pygame.image.load("bg.jpg")
         bg = pygame.transform.scale(bg, (800, 600))
         jet = pygame.image.load("jet.png").convert()
@@ -118,21 +123,41 @@ def how_to_play():
         enpic.set_colorkey(white)
         sp.set_colorkey(black)
         jet.set_colorkey(white)
-        screen.blit(bg, (0,0))
-        screen.blit(jet, (20,270))
+        screen.blit(bg, (0, 0))
+        screen.blit(jet, (20, 270))
         screen.blit(sp, (10, 70))
         screen.blit(enpic, (55, 200))
         screen.blit(enpic, (55, 190))
         screen.blit(enpic, (35, 190))
         screen.blit(enpic, (55, 180))
-        draw_text('How to play?', font, (255, 255, 255), screen, 308, 20)
-        draw_text('= Super Power - Gives you 5 seconds invincibility', htpfont, (255, 255, 255), screen, 90, 90)
-        draw_text('= Missiles - Avoid them and collect points 10 sec = 1 point', htpfont, (255, 255, 255), screen, 90, 180)
-        draw_text('= Player - You control it with arrow keys on your keyboard', htpfont, (255, 255, 255), screen, 90, 270)
- 
+        draw_text("How to play?", font, (255, 255, 255), screen, 308, 20)
+        draw_text(
+            "= Super Power - Gives you 5 seconds invincibility",
+            htpfont,
+            (255, 255, 255),
+            screen,
+            90,
+            90,
+        )
+        draw_text(
+            "= Missiles - Avoid them and collect points 10 sec = 1 point",
+            htpfont,
+            (255, 255, 255),
+            screen,
+            90,
+            180,
+        )
+        draw_text(
+            "= Player - You control it with arrow keys on your keyboard",
+            htpfont,
+            (255, 255, 255),
+            screen,
+            90,
+            270,
+        )
+
         mx, my = pygame.mouse.get_pos()
 
- 
         back_1 = pygame.Rect(302, 402, 200, 50)
         outlineback_1 = pygame.Rect(300, 400, 205, 55)
         if back_1.collidepoint((mx, my)):
@@ -140,10 +165,8 @@ def how_to_play():
                 main_menu()
         pygame.draw.rect(screen, (255, 255, 255), outlineback_1)
         pygame.draw.rect(screen, (255, 0, 0), back_1)
-        draw_text('Back', font, (255, 255, 255), screen, 365, 410)
+        draw_text("Back", font, (255, 255, 255), screen, 365, 410)
 
- 
- 
         click = False
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -155,25 +178,30 @@ def how_to_play():
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     click = True
- 
+
         pygame.display.update()
         clock.tick(60)
+
+
 def quit():
     pygame.quit()
     sys.exit()
+
+
 def game():
-    start_ticks=pygame.time.get_ticks()
+    start_ticks = pygame.time.get_ticks()
+
     class Player(pygame.sprite.Sprite):
         def __init__(self):
             super(Player, self).__init__()
             self.surf = pygame.image.load("jet.png").convert()
             self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-            #self.surf.fill((255, 255, 255))
+            # self.surf.fill((255, 255, 255))
             self.rect = self.surf.get_rect()
 
         def update(self, pressed_keys):
             if pressed_keys[K_UP]:
-                self.rect.move_ip(0, -10) #move in place
+                self.rect.move_ip(0, -10)  # move in place
                 move_up_sound.play()
             if pressed_keys[K_DOWN]:
                 self.rect.move_ip(0, 10)
@@ -200,7 +228,7 @@ def game():
             super(Enemy, self).__init__()
             self.surf = pygame.image.load("missile.png").convert()
             self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-            #self.surf.fill((255, 255, 255))
+            # self.surf.fill((255, 255, 255))
             self.rect = self.surf.get_rect(
                 center=(
                     random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
@@ -214,7 +242,7 @@ def game():
         def update(self):
             self.rect.move_ip(-self.speed, 0)
             if self.rect.right < 0:
-                self.kill()          
+                self.kill()
 
     # Define the cloud object by extending pygame.sprite.Sprite
     # Use an image for a better-looking sprite
@@ -237,6 +265,7 @@ def game():
             self.rect.move_ip(-5, 0)
             if self.rect.right < 0:
                 self.kill()
+
     # Define the cloud object by extending pygame.sprite.Sprite
     # Use an image for a better-looking sprite
     class SuperPower(pygame.sprite.Sprite):
@@ -257,11 +286,13 @@ def game():
             self.rect.move_ip(-5, 0)
             if self.rect.right < 0:
                 self.kill()
+
     # Create a custom event for adding a new enemy
 
     def superpowerdef():
         global superpowertimer
         superpowertimer = 0
+
     ADDENEMY = pygame.USEREVENT + 1
     pygame.time.set_timer(ADDENEMY, 165)
 
@@ -272,7 +303,7 @@ def game():
     pygame.time.set_timer(ENEMYSPEED, 10000)
 
     SUPERPOWER = pygame.USEREVENT + 4
-    pygame.time.set_timer(SUPERPOWER, random.randint(8500, 45000)) 
+    pygame.time.set_timer(SUPERPOWER, random.randint(8500, 45000))
 
     # Instantiate player. Right now, this is just a rectangle.
     player = Player()
@@ -295,6 +326,7 @@ def game():
     running = True
     pygame.mixer.music.load("Apoxode_-_Electric_1.mp3")
     pygame.mixer.music.play(loops=-1)
+    superboja = (0, 0, 0)
     # Main loop
     while running:
 
@@ -312,7 +344,7 @@ def game():
                     running = False
                     main_menu()
                     pygame.mixer.music.load("main_menu.mp3")
-                    pygame.mixer.music.play(loops=-1) 
+                    pygame.mixer.music.play(loops=-1)
 
             # Did the user click the window close button? If so, stop the loop.
             elif event.type == QUIT:
@@ -324,7 +356,7 @@ def game():
                 new_enemy = Enemy()
                 enemies.add(new_enemy)
                 all_sprites.add(new_enemy)
-                #Setting enemy speed after every 10 seconds, and player score every 10 seconds
+                # Setting enemy speed after every 10 seconds, and player score every 10 seconds
             elif event.type == ENEMYSPEED:
                 enemyspeed += random.randint(0, 7)
                 playerscore += 1
@@ -353,13 +385,13 @@ def game():
             screen.blit(entity.surf, entity.rect)
 
         seconds = 0
-        seconds += (pygame.time.get_ticks()-start_ticks)/1000
-        timer = 'Timer: ' + str(seconds)
+        seconds += (pygame.time.get_ticks() - start_ticks) / 1000
+        timer = "Timer: " + str(seconds)
         screen.blit(gamefont.render(timer, True, boja), (20, 5))
-        score = 'Score: ' + str(playerscore)
+        score = "Score: " + str(playerscore)
         screen.blit(gamefont.render(score, True, boja), (20, 45))
         supertext = "Super Power: " + str(superactive)
-        screen.blit(gamefont.render(supertext, True, boja), (20,85))
+        screen.blit(gamefont.render(supertext, True, superboja), (20, 85))
         # Check if any enemies have collided with the player
         if pygame.sprite.spritecollideany(player, superpowers):
             superpowertimer = 1
@@ -368,14 +400,16 @@ def game():
             t = threading.Timer(5.0, superpowerdef)
             t.start()
         if superpowertimer == 1:
-                    superactive = "ACTIVATED"
+            superactive = "ACTIVATED"
+            superboja = (0, 255, 0)
         else:
             superactive = "NOT ACTIVATED"
+            superboja = (255, 0, 0)
         if pygame.sprite.spritecollideany(player, enemies):
             # If so, then remove the player and stop the loop
             if superpowertimer == 0:
-#                lastscore = playerscore
- #               lasttime = seconds
+                #                lastscore = playerscore
+                #               lasttime = seconds
                 death_sound.play()
                 player.kill()
 
@@ -385,11 +419,11 @@ def game():
                 print("Time alive", seconds, "seconds")
                 print("Your score was", playerscore, "!")
                 # collChannel = collision_sound.play()
-                
+
                 running = False
                 main_menu()
                 pygame.mixer.music.load("main_menu.mp3")
-                pygame.mixer.music.play(loops=-1) 
+                pygame.mixer.music.play(loops=-1)
 
         pygame.display.update()
         # Ensure program maintains a rate of 30 frames per second
@@ -398,4 +432,6 @@ def game():
     #     pygame.time.wait(100)
 
     # All done! Stop and quit the mixer.
+
+
 main_menu()
